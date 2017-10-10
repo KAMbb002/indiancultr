506965ad543756b3d957f0a0021210b453068dca
Sanjeev Jha <tilkor.software@gmail.com>
2017-10-10 14:39:14 +0530

    sort by id test

--- public_html/app/code/core/Mage/Catalog/Block/Product/List/Toolbar.php
+++ public_html/app/code/core/Mage/Catalog/Block/Product/List/Toolbar.php
@@ -231,7 +231,7 @@ class Mage_Catalog_Block_Product_List_Toolbar extends Mage_Core_Block_Template
         }
         if ($this->getCurrentOrder()) {
           if(($this->getCurrentOrder())=='recentlyadded'){
-              $this->_collection->setOrder('entity_id','desc');
+              $this->_collection->setOrder('name','desc');
           }
           else {
            $this->_collection->setOrder($this->getCurrentOrder(),$this->getCurrentDirection());
