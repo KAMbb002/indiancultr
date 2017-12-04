2262a0aa860ca3fa1254892e6200afb28599eed4
Sanjeev Jha <tilkor.software@gmail.com>
2017-12-04 20:30:08 +0530

    Website Switecher

--- public_html/index.php
+++ public_html/index.php
@@ -101,7 +101,7 @@ else
 
 if($c_code=='IN')
 {
-	
+	echo "India".$c_code;
 	$allowedIps = Mage::getStoreConfig('dev/restrict/allow_ips');         
     $allowedIpsArray = explode(',', $allowedIps);     
     $currentIp = $_SERVER['REMOTE_ADDR'];    
@@ -117,6 +117,7 @@ if($c_code=='IN')
 }
 else
 {
+	echo "USA".$c_code;
    Mage::run('app_us', 'website');
 }
 
